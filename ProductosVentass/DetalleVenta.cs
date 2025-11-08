e39329d96d1b61a366c34f5593c063905a88df13
Julieth Angelica Cardenas Gómez <jcardenas@indigo.tech>
2025-11-08 10:21:32 -0500

    Se agregan las entidades.

---
+++ ProductosVentass/DetalleVenta.cs
@@ -0,0 +1,16 @@
+﻿namespace ProductosVentass
+{
+    public class DetalleVenta
+    {
+        public  required Guid Id { get; set; }
+        public required Guid ventaId { get; set; }
+        public required Guid productoId { get; set; }
+        public required decimal cantidadVendida { get; set; }
+        public required decimal precioUnitario { get; set; }
+        public required  decimal subtotal { get; set; }
+        public required decimal total { get; set; }
+
+        public required  Producto Producto { get; set; }
+        public required Ventas Ventas { get; set; }
+    }
+}
